1c2adb5c8afa89daaf826a37e91e33f5eb28ee9e
Nam Truong Le <nam.truong.le@sap.com>
2022-07-12 23:32:53 +0200

    first version

---
+++ pkg/mongodb/client.go
@@ -0,0 +1,66 @@
+package mongodb
+
+import (
+	"context"
+	"fmt"
+	"sync"
+
+	"github.com/sirupsen/logrus"
+	"github.com/vietnam-immigrations/go-utils/pkg/aws/ssm"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+var (
+	initClient sync.Once
+	client     *mongo.Client
+	db         string
+)
+
+func newClient(ctx context.Context, log *logrus.Entry, stage string) (*mongo.Client, error) {
+	initClient.Do(func() {
+		log.Infof("init mongodb client")
+		mongoHost, err := ssm.GetParameter(ctx, log, "vs2", "all", "/mongo/host", false)
+		if err != nil {
+			return
+		}
+		mongoUsername, err := ssm.GetParameter(ctx, log, "vs2", stage, "/mongo/username", false)
+		if err != nil {
+			return
+		}
+		mongoPassword, err := ssm.GetParameter(ctx, log, "vs2", stage, "/mongo/password", true)
+		if err != nil {
+			return
+		}
+		mongoDatabase, err := ssm.GetParameter(ctx, log, "vs2", stage, "/mongo/db", false)
+		if err != nil {
+			return
+		}
+		log.Infof("host = %s, user = %s, db = %s", mongoHost, mongoUsername, mongoDatabase)
+		mongoFullUrl := fmt.Sprintf("mongodb+srv://%s", mongoHost)
+
+		c, err := mongo.NewClient(
+			options.Client().ApplyURI(mongoFullUrl).SetAuth(options.Credential{
+				Username: mongoUsername,
+				Password: mongoPassword,
+			}),
+		)
+		if err != nil {
+			log.Errorf("failed to create mongodb client: %s", err)
+			return
+		}
+		err = c.Connect(ctx)
+		if err != nil {
+			log.Errorf("failed to connect to mongodb: %s", err)
+			return
+		}
+
+		client = c
+		db = mongoDatabase
+	})
+
+	if client == nil {
+		return nil, fmt.Errorf("failed to init mongodb client")
+	}
+	return client, nil
+}
---
+++ pkg/mongodb/mongo.go
@@ -0,0 +1,16 @@
+package mongodb
+
+import (
+	"context"
+
+	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+func Collection(ctx context.Context, log *logrus.Entry, stage string, name string) (*mongo.Collection, error) {
+	client, err := newClient(ctx, log, stage)
+	if err != nil {
+		return nil, err
+	}
+	return client.Database(db).Collection(name), nil
+}
