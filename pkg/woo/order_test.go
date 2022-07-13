a2c996bc0926eff835b17d56cb23fd06afd7cf8f
Nam Truong Le <nam.truong.le@sap.com>
2022-07-13 21:18:22 +0200

    test -short

--- pkg/woo/order_test.go
+++ pkg/woo/order_test.go
@@ -8,6 +8,9 @@ import (
 )
 
 func TestGetOrder(t *testing.T) {
+	if testing.Short() {
+		t.Skip()
+	}
 	_, err := GetOrder(context.TODO(), logrus.WithFields(nil), "dev", "110")
 	if err != nil {
 		t.Fail()
