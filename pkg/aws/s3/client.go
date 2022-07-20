16d72927b27e0a549bd2b55eb44091adfd412bc0
Nam Truong Le <nam.truong.le@sap.com>
2022-07-20 07:00:26 +0200

    s3 pre sign client

--- pkg/aws/s3/client.go
+++ pkg/aws/s3/client.go
@@ -14,6 +14,11 @@ var (
 	client     *s3.Client
 )
 
+var (
+	initPreSignClient sync.Once
+	preSignClient     *s3.PresignClient
+)
+
 func NewClient(ctx context.Context, log *logrus.Entry) (*s3.Client, error) {
 	var err error
 	initClient.Do(func() {
@@ -23,10 +28,29 @@ func NewClient(ctx context.Context, log *logrus.Entry) (*s3.Client, error) {
 			return
 		}
 		client = s3.NewFromConfig(cfg)
+		log.Info("s3 client created")
 	})
 	if err != nil {
+		log.Errorf("failed to create s3 client: %s", err)
 		return nil, err
 	}
-	log.Info("s3 client created")
 	return client, nil
+}
+
+func NewPreSignClient(ctx context.Context, log *logrus.Entry) (*s3.PresignClient, error) {
+	var err error
+	initPreSignClient.Do(func() {
+		_, e := NewClient(ctx, log)
+		if e != nil {
+			err = e
+			return
+		}
+		preSignClient = s3.NewPresignClient(client)
+		log.Info("s3 pre sign client created")
+	})
+	if err != nil {
+		log.Errorf("failed to create s3 pre sign client: %s", err)
+		return nil, err
+	}
+	return preSignClient, nil
 }
