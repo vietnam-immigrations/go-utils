f57913fa6303a7bb607120690c4258cac8b31538
Nam Truong Le <nam.truong.le@sap.com>
2022-09-03 09:36:15 +0200

    added product attribute keys

---
+++ pkg/woo/product.go
@@ -0,0 +1,16 @@
+package woo
+
+const (
+	ProductAttributePortrait         = "vs2_portrait"
+	ProductAttributePassportImage    = "vs2_passport_image"
+	ProductAttributeFirstName        = "vs2_first_name"
+	ProductAttributeLastName         = "vs2_last_name"
+	ProductAttributeDateOfBirth      = "vs2_date_of_birth"
+	ProductAttributeSex              = "vs2_sex"
+	ProductAttributeNationality      = "vs2_nationality"
+	ProductAttributePassportNumber   = "vs2_passport_number"
+	ProductAttributePassportExpiry   = "vs2_passport_expiry"
+	ProductAttributeRegistrationCode = "vs2_registration_code"
+
+	ProductSKUPriorityHandling = "V2"
+)
